af6a134eda6d3ec032bde3843951c02558fc5178
DualNixSix <H4L0R34CHFTW@HOTMAIL.COM>
2025-11-12 18:49:02 -0700

    Completed factorial python.

--- factorial.py
+++ factorial.py
@@ -1,3 +1,5 @@
 def factorial(num):
-	# your code here
-	pass
+	fact = 1
+	for f in range(1, num + 1):  # for loop iterates; range from 1 to num; num = 5 range is (1, 6): 1, 2, 3, 4, 5
+		fact *= f  # multiply fact by f and return result
+	return fact
