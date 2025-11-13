64668eb33e344a69b7bc35954f453af0aa16067b
DualNixSix <H4L0R34CHFTW@HOTMAIL.COM>
2025-11-12 19:29:45 -0700

    Changed 1 in for loop to 2.

--- factorial.py
+++ factorial.py
@@ -1,5 +1,5 @@
 def factorial(num):
 	fact = 1
-	for f in range(1, num + 1):  # for loop iterates; range from 1 to num; num = 5 range is (1, 6): 1, 2, 3, 4, 5
+	for f in range(2, num + 1):  # for loop iterates; range from 2 to num; num = 5 range is (2, 6): 2, 3, 4, 5
 		fact *= f  # multiply fact by f and return result
 	return fact
