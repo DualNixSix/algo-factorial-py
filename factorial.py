ac147c740c43eb80e913f63dd4ca9abaae91f1d1
DualNixSix <H4L0R34CHFTW@HOTMAIL.COM>
2025-11-12 19:34:02 -0700

    added back pass

--- factorial.py
+++ factorial.py
@@ -3,3 +3,4 @@ def factorial(num):
 	for f in range(2, num + 1):  # for loop iterates; range from 2 to num; num = 5 range is (2, 6): 2, 3, 4, 5
 		fact *= f  # multiply fact by f and return result
 	return fact
+pass
